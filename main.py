350c0bbd37b89149d57c6f27b745b21ee0ad3d68
Arthur Karoly <126285026+Azuma06@users.noreply.github.com>
2024-06-04 11:20:31 +0000

    error messages working now

--- main.py
+++ main.py
@@ -5,15 +5,19 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 TOKEN: Final = '7445691165:AAF3zQgRCky9mu_b8noFB9Ym6fFSVOYClHc'
 BOT_USERNAME: Final = '@secrrr_bot'
 
+
 # commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Olá, seja bem vinda ao chat bot do S. Beleza!')
 
+
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Eu sou um robô, digite algo que vou lhe ajudar!')
 
+
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('TODO: custom command.')
+
 
 # responses
 def handle_response(text: str) -> str:
@@ -27,29 +31,24 @@ def handle_response(text: str) -> str:
 
     return 'Eu nao entendi o que voce digitou...'
 
+
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    try:
-        message_type = update.effective_chat.type
-        text: str = update.message.text.lower()
+    text: str = update.message.text
+    chat_type: str = update.message.chat.type
 
-        print(f'User ({update.effective_user.id}) in {message_type}: "{text}"')
+    print(f'User ({update.message.chat.id}) in {chat_type}: "{text}"')
 
-        response: str = handle_response(text)
+    response: str = handle_response(text)
 
-        print('Bot:', response)
-        await update.message.reply_text(response)
-    except AttributeError as e:
-        print(f"Error in handle_message: {e}")
-        await update.effective_chat.send_message("I encountered an error processing your message.")
+    print('Bot:', response)
+    await update.message.reply_text(response)
+
 
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
-    if update.effective_chat:
-        print(f"Chat type: {update.effective_chat.type}")
-    else:
-        print("No chat information available")
 
-def main():
+
+if __name__ == "__main__":
     print('Starting bot...')
     app = Application.builder().token(TOKEN).build()
 
@@ -67,6 +66,3 @@ def main():
     # polls the bot
     print('Pensando...')
     app.run_polling(poll_interval=3)
-
-if __name__ == "__main__":
-    main()
