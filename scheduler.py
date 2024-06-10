3a94318565dbb23b5c90265dc1dc5efcec393e07
Azuma06 <arthurkaroly06@gmail.com>
2024-06-10 13:21:21 -0300

    wanna do this later maybe who knows

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,22 @@
-import schedule
-import time
-from main import send_appointment_reminders
-
-
-def job(t):
-    print('working', t)
-    send_appointment_reminders()
-    return
-
-
-def start_scheduler():
-    schedule.every().day.at('13:01').do(job, 'its 9:00')
-
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
-
-
-if __name__ == "__main__":
-    start_scheduler()
+# import schedule
+# import time
+# from main import send_appointment_reminders
+#
+#
+# def job(t):
+#     print('working', t)
+#     send_appointment_reminders()
+#     return
+#
+#
+# def start_scheduler():
+#     schedule.every().day.at('13:01').do(job, 'its 9:00')
+#
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(60)
+#
+#
+# if __name__ == "__main__":
+#     start_scheduler()
+#
